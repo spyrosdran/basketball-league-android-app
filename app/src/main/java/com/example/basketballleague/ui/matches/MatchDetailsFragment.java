37f40a958e792cc340ccd4e4b1af2b0b6d6b4d91
ElenPol <elen_pol@hotmail.com>
2022-06-02 15:58:58 +0300

    Live Match Details front ready

--- app/src/main/java/com/example/basketballleague/ui/matches/MatchDetailsFragment.java
+++ app/src/main/java/com/example/basketballleague/ui/matches/MatchDetailsFragment.java
@@ -1,5 +1,6 @@
 package com.example.basketballleague.ui.matches;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.basketballleague.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -43,7 +48,58 @@ public class MatchDetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_match_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_match_details, container, false);
+        LinearLayout linearLayout1 = view.findViewById(R.id.linearLayout1);
+        LinearLayout linearLayout3 = view.findViewById(R.id.linearLayout3);
+        final int N = 8; // total number of textviews to add
+        final TextView[] myTextViews = new TextView[N];
+        for (int i = 0; i < N; i++) {
+            // create a new textview
+            final TextView rowTextView = new TextView(getContext());
+
+            // setting text
+            rowTextView.setText(" #" + i);
+            // setting color to dynamic textviews
+            String whiteColorStr="#FFFFFF";
+            int whiteColor =  Color.parseColor(whiteColorStr);
+            rowTextView.setTextColor(whiteColor);
+            // setting margin to dynamic textviews
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(0,11,0,11);
+            rowTextView.setLayoutParams(params);
+            // setting size to dynamic textviews
+            rowTextView.setTextSize(20);
+            // add the textview to the linearlayout
+            linearLayout1.addView(rowTextView);
+
+            // save a reference to the textview for later
+            myTextViews[i] = rowTextView;
+        }
+
+        for (int i = 0; i < N; i++) {
+            // create a new textview
+            final TextView rowTextView = new TextView(getContext());
+
+            // setting text
+            rowTextView.setText(" #" + i);
+            // setting color to dynamic textviews
+            String whiteColorStr="#FFFFFF";
+            int whiteColor =  Color.parseColor(whiteColorStr);
+            rowTextView.setTextColor(whiteColor);
+            // setting margin to dynamic textviews
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(0,11,0,11);
+            rowTextView.setLayoutParams(params);
+            // setting size to dynamic textviews
+            rowTextView.setTextSize(20);
+            // add the textview to the linearlayout
+            linearLayout3.addView(rowTextView);
+
+            // save a reference to the textview for later
+            myTextViews[i] = rowTextView;
+        }
+
+
+        return view;
     }
 }
