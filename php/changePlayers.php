dac6edd051c1106fa28d176b8c3f0d1a53a1706a
Spyros Drantzios <spyrodran@gmail.com>
2022-06-15 20:57:18 +0300

    Changing a player updates the match table with the player's ID

--- php/changePlayers.php
+++ php/changePlayers.php
@@ -35,5 +35,25 @@
 	$sql = "INSERT INTO `match_event` (`playerID`, `matchID`, `type`, `minute`) VALUES(" . $changeInPlayerID . "," . $matchID . ",'change in', '" . $date . "');";
 	mysqli_query($dbh, $sql);
 	
+	//Updating match table
+	$query = "SELECT `home1`,`home2`,`home3`,`home4`,`home5`,`away1`, `away2`, `away3`, `away4`, `away5` FROM `match` WHERE `matchID` = " . $matchID . ";";
+	$result = $mysqli->query($query);
+	$row = $result->fetch_assoc();
+	$column = "";
+	
+	if($row["home1"] == $changeOutPlayerID) $column = "home1";
+	if($row["home2"] == $changeOutPlayerID) $column = "home2";
+	if($row["home3"] == $changeOutPlayerID) $column = "home3";
+	if($row["home4"] == $changeOutPlayerID) $column = "home4";
+	if($row["home5"] == $changeOutPlayerID) $column = "home5";
+	if($row["away1"] == $changeOutPlayerID) $column = "away1";
+	if($row["away2"] == $changeOutPlayerID) $column = "away2";
+	if($row["away3"] == $changeOutPlayerID) $column = "away3";
+	if($row["away4"] == $changeOutPlayerID) $column = "away4";
+	if($row["away5"] == $changeOutPlayerID) $column = "away5";
+	
+	$sql = "UPDATE `match` SET `" . $column ."` = '" . $changeInPlayerID . "' WHERE `matchID` = " . $matchID . ";";
+	mysqli_query($dbh, $sql);
+	
 	mysqli_close($dbh);
 ?>
